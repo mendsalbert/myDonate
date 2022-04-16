74f4fe0aee511262e6949c23cf31b6096e562a86
graphica99 <mendsalbert@gmail.com>
2022-04-16 18:41:16 +0000

    ghla

--- pages/dashboard/admin.tsx
+++ pages/dashboard/admin.tsx
@@ -3,10 +3,13 @@ export default function Dashboard() {
     <>
       <div className="bg-white flex flex-col font-Montserrat ">
         <div className="w-full h-32 bg-[#008E89] px-20 ">
-          <div className="flex flex-row">
-            <div className="flex flex-row justify-center items-center  space-x-3">
+          <div className="flex flex-row justify-between py-5">
+            <div className="flex flex-row justify-center items-center  space-x-2">
               <img src="/images/ghlalogo.png" className="w-14 " />
               <p className="text-white text-xl">GHLA</p>
+            </div>
+            <div className=" cursor-pointer rounded-full px-16 w-full bg-white  text-[#008E89]">
+              <p>Register</p>
             </div>
           </div>
         </div>
