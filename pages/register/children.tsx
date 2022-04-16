cd8f75615cdd8fc08b67c6506d89c811bba5fef4
graphica99 <mendsalbert@gmail.com>
2022-04-16 09:16:55 +0000

    ghla

--- pages/register/children.tsx
+++ pages/register/children.tsx
@@ -111,6 +111,12 @@ const Children = (props: Props) => {
                   placeholder="90210"
                 />
               </div>
+              <button
+                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                type="button"
+              >
+                Sign Up
+              </button>
             </div>
           </form>
         </div>
