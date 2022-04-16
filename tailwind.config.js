fcf377c66992227b302e2c8b69d91dd54545683d
graphica99 <mendsalbert@gmail.com>
2022-04-16 08:10:26 +0000

    ghla

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrat: "'Montserrat', sans-serif",
+        LeagueGothis: "'League Gothic', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
