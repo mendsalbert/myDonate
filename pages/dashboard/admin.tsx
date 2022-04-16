dad6097a6595edc9cb233578ff6f4d4eb6111e02
graphica99 <mendsalbert@gmail.com>
2022-04-16 18:29:36 +0000

    ghla

--- pages/dashboard/admin.tsx
+++ pages/dashboard/admin.tsx
@@ -1,8 +1,8 @@
 export default function Dashboard() {
   return (
     <>
-      <div className="bg-white flex flex-col px-20">
-        <div className="w-full h-32 bg-[#008E89]"></div>
+      <div className="bg-white flex flex-col ">
+        <div className="w-full h-32 bg-[#008E89] px-20 flex flex-row"></div>
         <div></div>
       </div>
     </>
