d356058eca8463bfc1dec05ead0fc96e27fc1013
graphica99 <mendsalbert@gmail.com>
2022-04-16 09:17:40 +0000

    ghla

--- pages/register/children.tsx
+++ pages/register/children.tsx
@@ -111,13 +111,13 @@ const Children = (props: Props) => {
                   placeholder="90210"
                 />
               </div>
-              <button
-                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                type="button"
-              >
-                Sign Up
-              </button>
             </div>
+            <button
+              className="flex-shrink-0 bg-teal-500 w-full hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-3 px-2 rounded"
+              type="button"
+            >
+              Sign Up
+            </button>
           </form>
         </div>
       </div>
