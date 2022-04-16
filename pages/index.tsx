ae06ff1a2b463140689845ca2f5a3e692b8ef099
graphica99 <mendsalbert@gmail.com>
2022-04-16 01:25:05 +0000

    ghla

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+  // <Layout title="Home | Next.js + TypeScript Example">
+  <div className="bg-[#008E89] w-full h-screen">
+    <img src="/images/ghlalogo.png" />
+  </div>
+  // </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
