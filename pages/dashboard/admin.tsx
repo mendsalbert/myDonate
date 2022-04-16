8b9390aeae08c99b60c12876676d147773c3a1f7
graphica99 <mendsalbert@gmail.com>
2022-04-16 18:28:18 +0000

    ghla

--- pages/dashboard/admin.tsx
+++ pages/dashboard/admin.tsx
@@ -1,3 +1,10 @@
 export default function Dashboard() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-white flex flex-col px-20">
+        <div className="w-full h-32 bg-[#008E89]"></div>
+        <div></div>
+      </div>
+    </>
+  );
 }
