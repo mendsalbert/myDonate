8626c9f98dc3d175a5918c9cdb24fd232aebe957
graphica99 <mendsalbert@gmail.com>
2022-04-16 08:45:59 +0000

    ghla

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const IndexPage = () => (
 
       {/* <div> */}
       <div className=" z-10 bg-white w-max mx-6 md:mx-0 rounded-2xl shadow-2xl ">
-        <div className="flex flex-col md:flex-row md:space-x-9 space-x-0 space-y-5 md:space-y-0  p-8 md:p-16 ">
+        <div className="flex flex-col md:flex-row md:space-x-9 space-x-0 space-y-5 md:space-y-0  p-7 md:p-16 ">
           <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
             <img src="/images/children.svg" className="w-24" />
             <p className="text-3xl text-[#008E89] ">Children</p>
