99fe37c0aa482af4d3ec8bbf0765d2ed45522348
graphica99 <mendsalbert@gmail.com>
2022-04-16 08:25:38 +0000

    ghla

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,21 +16,26 @@ const IndexPage = () => (
         <p>Lending Services</p>
       </div>
 
-      <div className="bg-white rounded-2xl z-10 grid grid-cols-2 gap-10 p-16 ">
-        <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
-          <img src="/images/children.svg" className="w-24" />
-          <p className="text-3xl text-[#008E89] ">Children</p>
-          <div className="rounded-full px-16 w-full text-white py-2 bg-[#008E89]">
-            <p>Register</p>
+      <div>
+        <div className="bg-white rounded-2xl z-10 grid grid-cols-2 gap-10 p-16 ">
+          <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
+            <img src="/images/children.svg" className="w-24" />
+            <p className="text-3xl text-[#008E89] ">Children</p>
+            <div className="rounded-full px-16 w-full text-white py-2 bg-[#008E89]">
+              <p>Register</p>
+            </div>
+          </div>
+          <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
+            <img src="/images/adult.svg" className="w-24" />
+            <p className="text-3xl text-[#008E89] ">Adult</p>
+            <div className="rounded-full px-16 w-full text-white py-2 bg-[#008E89]">
+              <p>Register</p>
+            </div>
           </div>
         </div>
-        <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
-          <img src="/images/children.svg" className="w-24" />
-          <p className="text-3xl text-[#008E89] ">Children</p>
-          <div className="rounded-full px-16 w-full text-white py-2 bg-[#008E89]">
-            <p>Register</p>
-          </div>
-        </div>
+        <p className="text-center text-gray-700 text-xl">
+          Already have an account ? Log In
+        </p>
       </div>
     </div>
   </div>
