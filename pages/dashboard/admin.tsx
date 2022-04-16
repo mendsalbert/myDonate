45c3f450712bc7eae4d8c8936c0d1430c421f4bb
graphica99 <mendsalbert@gmail.com>
2022-04-16 18:36:27 +0000

    ghla

--- pages/dashboard/admin.tsx
+++ pages/dashboard/admin.tsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
         <div className="w-full h-32 bg-[#008E89] px-20 ">
           <div className="flex flex-row">
             <div className="flex flex-row justify-center items-center  space-x-3">
-              <img src="/images/ghlalogo.png" className="w-14 mt-5" />
+              <img src="/images/ghlalogo.png" className="w-14 " />
               <p className="text-white text-xl">GHLA</p>
             </div>
           </div>
