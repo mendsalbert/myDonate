85304cbd9faa19c52955d3ea4ff14cb359613c82
graphica99 <mendsalbert@gmail.com>
2022-04-16 22:55:52 +0000

    ghla

--- pages/dashboard/admin/books.tsx
+++ pages/dashboard/admin/books.tsx
@@ -3,52 +3,18 @@ import {
   BookOpenIcon,
   UserGroupIcon,
 } from "@heroicons/react/outline";
+import Layout from "../../../components/adminLayout";
 export default function Dashboard() {
   return (
     <>
-      <div className="bg-white  font-Montserrat ">
-        <div className="bg-[#008E89] px-10 md:px-36 ">
-          <div className="flex flex-row justify-between py-5">
-            <div className="flex flex-row justify-center items-center  space-x-2">
-              <img src="/images/ghlalogo.png" className="w-10 " />
-              <p className="text-white text-lg">GHLA</p>
-            </div>
-            <p className="bg-white text-center rounded-full text-lg py-1 px-5 text-[#008E89]">
-              Log out
-            </p>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-3 text-center gap-7 py-16">
-            <div className="bg-white shadow-2xl flex flex-col items-center justify-center space-y-2 py-10">
-              <div className="bg-yellow-600 rounded-full p-3 ">
-                <UserGroupIcon className="w-10 text-white" />
-              </div>
-              <p className="text-2xl">USERS</p>
-              <p className=" text-2xl text-gray-600">5 Users added</p>
-            </div>
-            <div className="bg-white shadow-2xl flex flex-col items-center justify-center space-y-2 py-10">
-              <div className="bg-green-600 rounded-full p-3 ">
-                <BookOpenIcon className="w-10 text-white" />
-              </div>
-              <p className="text-2xl">BOOKS</p>
-              <p className=" text-2xl text-gray-600">10 Books added</p>
-            </div>
-            <div className="bg-white shadow-2xl flex flex-col items-center justify-center space-y-2 py-10">
-              <div className="bg-red-600 rounded-full p-3 ">
-                <BookOpenIcon className="w-10 text-white" />
-              </div>
-              <p className="text-2xl">OVERDUE</p>
-              <p className=" text-2xl text-gray-600">20 Books overdue</p>
-            </div>
-          </div>
-        </div>
+      <Layout>
         <div className="w-full  bg-white px-10 md:px-36 ">
           <body className="antialiased font-sans ">
             <div className="container mx-auto  ">
               <div className="py-8">
                 <div>
                   <h2 className="text-3xl text-gray-600  leading-tight">
-                    Users
+                    Books
                   </h2>
                 </div>
                 <div className="flex flex-col md:flex-row md:justify-between  md:items-center">
@@ -103,7 +69,7 @@ export default function Dashboard() {
                     </div>
                   </div>
                   <div className="bg-blue-600 text-lg text-center px-6 py-1 rounded-md cursor-pointer text-white">
-                    Add User
+                    Add Book
                   </div>
                 </div>
                 <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
@@ -295,7 +261,7 @@ export default function Dashboard() {
             </div>
           </body>
         </div>
-      </div>
+      </Layout>
     </>
   );
 }
