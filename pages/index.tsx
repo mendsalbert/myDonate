6706c3bdec41f2a388421f107c5cdb76ac754774
graphica99 <mendsalbert@gmail.com>
2022-04-16 08:33:48 +0000

    ghla

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   // <Layout title="Home | Next.js + TypeScript Example">
-  <div className="bg-[#008E89] w-full h-screen relative font-Montserrat">
+  <div className="bg-[#008E89] w-full h-full relative font-Montserrat">
     <div className="absolute left-0 bottom-0 -z-5">
       <img src="/images/undraw_books_re_8gea.svg" className="w-96" />
     </div>
@@ -18,7 +18,7 @@ const IndexPage = () => (
 
       {/* <div> */}
       <div className=" z-10 bg-white rounded-2xl shadow-2xl ">
-        <div className="flex flex-row space-x-9 p-16 ">
+        <div className="flex flex-col md:flex-row md:space-x-9 space-x-0 p-16 ">
           <div className="border-2 border-[#008E89] rounded-2xl text-center flex flex-col items-center space-y-4  p-4  py-5">
             <img src="/images/children.svg" className="w-24" />
             <p className="text-3xl text-[#008E89] ">Children</p>
