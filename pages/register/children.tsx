599b51a8f6a63e26a202febe72a035d3b69edac0
graphica99 <mendsalbert@gmail.com>
2022-04-17 19:00:35 +0000

    ghla

--- pages/register/children.tsx
+++ pages/register/children.tsx
@@ -131,9 +131,19 @@ const Children = (props: Props) => {
                   type="password"
                   placeholder="******************"
                 />
-                <p className="text-gray-600 text-xs italic">
-                  Make it as long and as crazy as you'd like
-                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                  Confirm Password
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-password"
+                  type="password"
+                  placeholder="******************"
+                />
               </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-2">
