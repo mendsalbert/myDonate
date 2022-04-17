7cfabb40b811a9a72ad0926faef716c517ec292d
graphica99 <mendsalbert@gmail.com>
2022-04-17 22:38:59 +0000

    ghla

--- pages/dashboard/admin/index.tsx
+++ pages/dashboard/admin/index.tsx
@@ -26,6 +26,7 @@ export default function Dashboard() {
                       </div>
                       <div className="relative">
                         <select className=" h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block  w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+                          <option>All</option>
                           <option>Adults</option>
                           <option>Children</option>
                         </select>
@@ -57,7 +58,6 @@ export default function Dashboard() {
                   <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
                     <table className="min-w-full leading-normal">
                       <thead>
-                        image -- name -- contact -- status --gps address
                         <tr>
                           <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                             User
@@ -114,139 +114,36 @@ export default function Dashboard() {
                             </span>
                           </td>
                           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <p className="text-gray-600 text-md py-1">Book 1</p>
-                            <span className="relative inline-block cursor-pointer px-3 py-1 font-semibold text-green-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Returned</span>
-                            </span>
-                            <span className="relative inline-block cursor-pointer px-3 py-1 font-semibold text-yellow-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-yellow-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Renewed</span>
-                            </span>
-                            <span className="relative inline-block cursor-pointer px-3 py-1 font-semibold text-red-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Lost</span>
-                            </span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <div className="flex items-center">
-                              <div className="flex-shrink-0 w-10 h-10">
-                                <img
-                                  className="w-full h-full rounded-full"
-                                  src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                  alt=""
-                                />
-                              </div>
-                              <div className="ml-3">
-                                <p className="text-gray-900 whitespace-no-wrap">
-                                  Blake Bowman
-                                </p>
-                              </div>
+                            <div className="flex flex-row items-center  space-x-2">
+                              <p className="text-gray-600 text-md py-1">
+                                Book 1
+                              </p>
+                              <span className="relative inline-block cursor-pointer px-3 py-1 font-semibold text-green-900 leading-tight">
+                                <span
+                                  aria-hidden
+                                  className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                                ></span>
+                                <span className="relative">Returned</span>
+                              </span>
                             </div>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Editor
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Jan 01, 2020
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Activo</span>
-                            </span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <div className="flex items-center">
-                              <div className="flex-shrink-0 w-10 h-10">
-                                <img
-                                  className="w-full h-full rounded-full"
-                                  src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                  alt=""
-                                />
-                              </div>
-                              <div className="ml-3">
-                                <p className="text-gray-900 whitespace-no-wrap">
-                                  Dana Moore
-                                </p>
-                              </div>
+
+                            <div className="flex flex-row items-center space-x-2">
+                              {/* <p className="text-gray-600 text-md py-1"> */}
+                              <a href="#" className="text-blue-600 underline">
+                                Book 1
+                              </a>
+                              {/* </p> */}
+
+                              <span className="relative inline-block cursor-pointer px-3 py-1 font-semibold text-red-900 leading-tight">
+                                <span
+                                  aria-hidden
+                                  className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+                                ></span>
+                                <span className="relative">
+                                  expired (24/05/22)
+                                </span>
+                              </span>
                             </div>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Editor
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Jan 10, 2020
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                            <span className="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Suspended</span>
-                            </span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td className="px-5 py-5 bg-white text-sm">
-                            <div className="flex items-center">
-                              <div className="flex-shrink-0 w-10 h-10">
-                                <img
-                                  className="w-full h-full rounded-full"
-                                  src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
-                                  alt=""
-                                />
-                              </div>
-                              <div className="ml-3">
-                                <p className="text-gray-900 whitespace-no-wrap">
-                                  Alonzo Cox
-                                </p>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="px-5 py-5 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Admin
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 bg-white text-sm">
-                            <p className="text-gray-900 whitespace-no-wrap">
-                              Jan 18, 2020
-                            </p>
-                          </td>
-                          <td className="px-5 py-5 bg-white text-sm">
-                            <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
-                              <span
-                                aria-hidden
-                                className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                              ></span>
-                              <span className="relative">Inactive</span>
-                            </span>
                           </td>
                         </tr>
                       </tbody>
