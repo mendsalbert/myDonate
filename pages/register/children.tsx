56978a0a0433a3a5fd189e2b8fd5b2d09cac5219
graphica99 <mendsalbert@gmail.com>
2022-04-17 21:57:25 +0000

    ghla

--- pages/register/children.tsx
+++ pages/register/children.tsx
@@ -1,17 +1,49 @@
 import { CameraIcon } from "@heroicons/react/outline";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 type Props = {};
 
 const Children = (props: Props) => {
+  const fileRef = useRef() as any;
   const [file, setfile] = useState("") as any;
+  const [firstname, setfirstname] = useState("Mends");
+  const [lastname, setlastname] = useState("Albert");
+  const [firstguardian, setfirstguardian] = useState("martha");
+  const [secondguardian, setsecondguardian] = useState("joseph");
+  const [firstguardiancontact, setfirstguardiancontact] =
+    useState("0249107812");
+  const [secondguardiancontact, setsecondguardiancontact] =
+    useState("0249107812");
+  const [schoolname, setschoolname] = useState("divine");
+  const [classname, setclassname] = useState("class 6");
+  const [gpsaddress, setgpsaddress] = useState("ba");
+  const [housenumber, sethousenumber] = useState("sp90");
+  const [password, setpassword] = useState("1234567");
+  const [confirmpassword, setconfirmpassword] = useState("1234567");
+
   const handleChange = (event) => {
     setfile({
       file: URL.createObjectURL(event.target.files[0]),
     });
   };
 
-  console.log(file);
+  const onSubmitHandler = () => {
+    if (password !== confirmpassword) {
+      alert("password do not match");
+    }
+    console.log(firstname);
+    console.log(lastname);
+    console.log(firstguardian);
+    console.log(secondguardian);
+    console.log(firstguardiancontact);
+    console.log(secondguardiancontact);
+    console.log(schoolname);
+    console.log(classname);
+    console.log(gpsaddress);
+    console.log(housenumber);
+    console.log(password);
+    console.log(confirmpassword);
+  };
   return (
     <div className="bg-[#008E89] w-full min-h-screen relative  font-Montserrat">
       <div className="absolute left-0 bottom-0 -z-5">
@@ -27,24 +59,26 @@ const Children = (props: Props) => {
           </p>
           <p className="text-white text-lg md:text-2xl">Lending Services</p>
         </div>
-        <div className=" z-10 bg-white mx-2 md:mx-0 mb-10 rounded-2xl shadow-2xl p-6  ">
-          <form className="w-full max-w-lg">
+        <div className=" z-10 bg-white mx-2 md:mx-0 mb-10 rounded-2xl shadow-2xl p-6  overflow-hidden w-full md:w-8/12 ">
+          <form className="w-full  ">
             <p className="text-gray-700 text-xl text-center pb-6 ">
-              {" "}
               Children Registration Form
             </p>
 
             <div>
-              <img src={file.file} className="w-24 h-24" />
+              <img src={file.file} className="w-36" />
+              <button onClick={() => fileRef.current.click()}>
+                Custom File Input Button
+              </button>
               <div className="relative">
                 <input
+                  ref={fileRef}
                   type="file"
                   onChange={(e) => {
                     handleChange(e);
                   }}
-                  className="opacity-0 z-50 "
+                  className="opacity z-50"
                 />
-                <CameraIcon className="h-12 z-10 absolute top-0" />
               </div>
             </div>
             <div className="flex flex-wrap -mx-3 mb-6">
@@ -55,6 +89,10 @@ const Children = (props: Props) => {
                 <input
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                   id="grid-first-name"
+                  name={firstname}
+                  onChange={(e) => {
+                    setfirstname(e.target.value);
+                  }}
                   type="text"
                   placeholder="Jane"
                 />
@@ -67,6 +105,10 @@ const Children = (props: Props) => {
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   id="grid-last-name"
                   type="text"
+                  name={lastname}
+                  onChange={(e) => {
+                    setlastname(e.target.value);
+                  }}
                   placeholder="Doe"
                 />
               </div>
@@ -81,6 +123,10 @@ const Children = (props: Props) => {
                   id="grid-first-name"
                   type="text"
                   placeholder="Jane"
+                  name={firstguardian}
+                  onChange={(e) => {
+                    setfirstguardian(e.target.value);
+                  }}
                 />
               </div>
               <div className="w-full md:w-1/2 px-3">
@@ -92,6 +138,10 @@ const Children = (props: Props) => {
                   id="grid-last-name"
                   type="text"
                   placeholder="020*******"
+                  name={firstguardiancontact}
+                  onChange={(e) => {
+                    setfirstguardiancontact(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -105,6 +155,10 @@ const Children = (props: Props) => {
                   id="grid-first-name"
                   type="text"
                   placeholder="Jane"
+                  name={secondguardian}
+                  onChange={(e) => {
+                    setsecondguardian(e.target.value);
+                  }}
                 />
               </div>
               <div className="w-full md:w-1/2 px-3">
@@ -116,6 +170,10 @@ const Children = (props: Props) => {
                   id="grid-last-name"
                   type="text"
                   placeholder="020*******"
+                  name={secondguardiancontact}
+                  onChange={(e) => {
+                    setsecondguardiancontact(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -129,6 +187,10 @@ const Children = (props: Props) => {
                   id="grid-first-name"
                   type="text"
                   placeholder="Divine international school"
+                  name={schoolname}
+                  onChange={(e) => {
+                    setschoolname(e.target.value);
+                  }}
                 />
               </div>
               <div className="w-full md:w-1/2 px-3">
@@ -140,6 +202,10 @@ const Children = (props: Props) => {
                   id="grid-last-name"
                   type="text"
                   placeholder="Class 6"
+                  name={classname}
+                  onChange={(e) => {
+                    setclassname(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -153,6 +219,10 @@ const Children = (props: Props) => {
                   id="grid-city"
                   type="text"
                   placeholder="BA-60-55"
+                  name={gpsaddress}
+                  onChange={(e) => {
+                    setgpsaddress(e.target.value);
+                  }}
                 />
               </div>
               <div className="w-full md:w-6/12 px-3 mb-6 md:mb-0">
@@ -164,6 +234,10 @@ const Children = (props: Props) => {
                   id="grid-city"
                   type="text"
                   placeholder="SP 98"
+                  name={housenumber}
+                  onChange={(e) => {
+                    sethousenumber(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -177,6 +251,10 @@ const Children = (props: Props) => {
                   id="grid-password"
                   type="password"
                   placeholder="******************"
+                  name={password}
+                  onChange={(e) => {
+                    setpassword(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -190,6 +268,10 @@ const Children = (props: Props) => {
                   id="grid-password"
                   type="password"
                   placeholder="******************"
+                  name={confirmpassword}
+                  onChange={(e) => {
+                    setconfirmpassword(e.target.value);
+                  }}
                 />
               </div>
             </div>
@@ -197,6 +279,9 @@ const Children = (props: Props) => {
             <button
               className="flex-shrink-0 bg-teal-500 w-full hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-2 mt-4 px-2 rounded"
               type="button"
+              onClick={() => {
+                onSubmitHandler();
+              }}
             >
               Sign Up
             </button>
