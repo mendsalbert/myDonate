2f5d6d9639309f31d3086c15b0fb0423f3610d3d
graphica99 <mendsalbert@gmail.com>
2022-04-18 14:19:43 +0000

    ghla

--- pages/dashboard/admin/books.tsx
+++ pages/dashboard/admin/books.tsx
@@ -33,14 +33,27 @@ export default function Book() {
                   <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-full px-3 mb-6 md:mb-0">
                       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        Book title
+                        User
                       </label>
-                      <input
-                        className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-first-name"
-                        type="text"
-                        placeholder="Book title"
-                      />
+                      <div className="relative">
+                        <select
+                          className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="grid-state"
+                        >
+                          <option></option>
+                          <option>Mends Albert</option>
+                          <option>Mavis Mawusi</option>
+                        </select>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                          <svg
+                            className="fill-current h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                          >
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                          </svg>
+                        </div>
+                      </div>
                     </div>
                     <div className="w-full md:w-full px-3">
                       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
