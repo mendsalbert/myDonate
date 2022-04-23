c0be74ffebba69953c79d71a7f55145a1812aa6c
graphica99 <mendsalbert@gmail.com>
2022-04-23 23:48:06 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <div className="mx-40 my-6">
+  <div className="mx-40 my-6 font-Montserrat">
     <div className="flex flex-row justify-between items-center space-x-32">
       <img src="/images/logo.png" className="w-28 " />
       <input type="text" className="border-2 rounded-lg grow" />
