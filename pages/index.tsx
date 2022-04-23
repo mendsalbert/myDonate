c55bf654c8e4d545d864ebb5d61a6af0b4578a84
graphica99 <mendsalbert@gmail.com>
2022-04-23 23:57:26 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <div className="flex flex-row justify-between items-center space-x-32">
       <img src="/images/logo.png" className="w-28 " />
       <div className="flex grow flex-row items-center bg-[#E7DDDD] px-4 rounded-full">
-        <SearchIcon className="h-7 text-gray-500" />
+        <SearchIcon className="h-6 text-gray-500" />
         <input
           type="text"
           className=" py-2 bg-[#E7DDDD] ring-0 focus:ring-0 outline-none focus:outline-none rounded-full px-4"
@@ -16,6 +16,13 @@ const IndexPage = () => (
       <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-2 rounded-md cursor-pointer text-white">
         Connet Wallet
       </div>
+    </div>
+    <div className="flex flex-row justify-between">
+      <p>
+        The most transparent donation platform on the internet. Donate in
+        cryptos
+      </p>
+      <div></div>
     </div>
   </div>
 );
