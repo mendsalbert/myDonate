c5c0643e956407aa63a68a17bbff74a17420d0c2
graphica99 <mendsalbert@gmail.com>
2022-04-23 23:30:19 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 
 const IndexPage = () => (
-  <div className="mx-40">
-    <p>hello world</p>
+  <div className="mx-40 my-6">
+    <div>
+      <img src="/images/logo.png" className="w-28" />
+      <input type="search" />
+    </div>
   </div>
 );
 
