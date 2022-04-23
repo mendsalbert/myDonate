b2f60dd555898d9ed3b57f00555c3278d6d457a7
graphica99 <mendsalbert@gmail.com>
2022-04-23 23:53:04 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { SearchCircleIcon, SearchIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
@@ -5,10 +6,13 @@ const IndexPage = () => (
   <div className="mx-40 my-6 font-Montserrat">
     <div className="flex flex-row justify-between items-center space-x-32">
       <img src="/images/logo.png" className="w-28 " />
-      <input
-        type="text"
-        className="border-2 grow py-2 bg-[#E7DDDD]  outline-none focus:outline-none rounded-full px-4"
-      />
+      <div>
+        <SearchIcon className="h-7" />
+        <input
+          type="text"
+          className="border-2 grow py-2 bg-[#E7DDDD]  outline-none focus:outline-none rounded-full px-4"
+        />
+      </div>
       <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-2 rounded-md cursor-pointer text-white">
         Connet Wallet
       </div>
