e265dd79731c88d7d4a74f41774fe8c76eedf16b
graphica99 <mendsalbert@gmail.com>
2022-04-23 23:34:31 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <div className="mx-40 my-6">
-    <div className="flex flex-row justify-between items-center">
+    <div className="flex flex-row justify-between items-center space-x-32">
       <img src="/images/logo.png" className="w-28 " />
       <input type="text" className="border-2 rounded-lg grow" />
       <div>Connet Wallet</div>
