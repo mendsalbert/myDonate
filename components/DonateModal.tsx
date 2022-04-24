89394e7f491825e9407331b9179a693bfd1ad3af
graphica99 <mendsalbert@gmail.com>
2022-04-24 16:57:42 +0000

    my donate

--- components/DonateModal.tsx
+++ components/DonateModal.tsx
@@ -4,74 +4,10 @@ type Props = {};
 
 const DonateModal = (props: Props) => {
   return (
-    <div>
-      <button
-        id="dropdownDefault"
-        data-dropdown-toggle="dropdown"
-        className="text-white  bg-blue-700 w-full hover:bg-blue-800  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-between"
-        type="button"
-      >
-        Dropdown button{" "}
-        <svg
-          className="ml-2 w-4 h-4"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
-      </button>
-
-      <div
-        id="dropdown"
-        className=" z-10 text-center w-full bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-        data-popper-placement="top"
-        // style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(350px, -36px);"
-      >
-        <ul
-          className="py-3 my-3 text-sm text-gray-700 bg-white shadow-xl"
-          aria-labelledby="dropdownDefault"
-        >
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div className="">
+      <p className="text-center text-gray-500 text-lg">
+        How much you wanna donate?
+      </p>
     </div>
   );
 };
