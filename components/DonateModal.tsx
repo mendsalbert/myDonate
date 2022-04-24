2595cce9babb16a0f1f096cf336b15e97a16a52e
graphica99 <mendsalbert@gmail.com>
2022-04-24 16:59:45 +0000

    my donate

--- components/DonateModal.tsx
+++ components/DonateModal.tsx
@@ -8,6 +8,11 @@ const DonateModal = (props: Props) => {
       <p className="text-center text-gray-500 text-lg">
         How much you wanna donate?
       </p>
+      <div className="p-5">
+        <div className="text-center border-2 border-gray-600 rounded-lg w-full">
+          5 ETH
+        </div>
+      </div>
     </div>
   );
 };
