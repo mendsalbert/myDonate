f5a9857ee7c1c8232a78b733b44c83e7a69b04b6
graphica99 <mendsalbert@gmail.com>
2022-04-24 12:46:44 +0000

    my donate

--- components/DonateModal.tsx
+++ components/DonateModal.tsx
@@ -8,7 +8,7 @@ const DonateModal = (props: Props) => {
       <button
         id="dropdownDefault"
         data-dropdown-toggle="dropdown"
-        className="text-white bg-blue-700 w-full hover:bg-blue-800  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white  bg-blue-700 w-full hover:bg-blue-800  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 justify-between"
         type="button"
       >
         Dropdown button{" "}
@@ -30,7 +30,7 @@ const DonateModal = (props: Props) => {
 
       <div
         id="dropdown"
-        className=" z-10 w-full bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+        className=" z-10 text-center w-full bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
         data-popper-placement="top"
         // style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(350px, -36px);"
       >
