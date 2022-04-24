e70159eef607d5d696ea5754abfc72310b4b3d31
graphica99 <mendsalbert@gmail.com>
2022-04-24 00:02:42 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,8 @@ const IndexPage = () => (
         The most transparent donation platform on the internet. Donate in
         cryptos
       </p>
-      <div className="w-7/12  bg-gradient-to-r p-8 from-cyan-500 text-center rounded-md to-blue-500">
-        <p>Start New Fundraising</p>
+      <div className="w-6/12  bg-gradient-to-r p-8 from-cyan-500 text-center rounded-md to-blue-500">
+        <p className="text-white font-bold">Start New Fundraising</p>
         <div className="bg-white px-6 py-2 rounded-md cursor-pointer text-blue">
           Connet Wallet
         </div>
