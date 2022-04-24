5734bf991f9104878731f6f424b65e461f8736f9
graphica99 <mendsalbert@gmail.com>
2022-04-24 09:54:53 +0000

    my donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -182,12 +182,12 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
-    <div className="mt-28 text-center w-full flex flex-col justify-center items-center">
+    <div className="mt-28 text-center w-full py-4 flex flex-col justify-center items-center">
       <p className="text-lg text-gray-500 ">Connet with us</p>
-      <div className="flex flex-row">
+      <div className="flex flex-row space-x-4 cursor-pointer">
         <img src="/images/twitter.svg" className="w-7" />
         <img src="/images/discord.svg" className="w-10" />
-        <img src="/images/instagram.svg" className="w-10" />
+        <img src="/images/instagram.svg" className="w-7" />
       </div>
     </div>
   </div>
