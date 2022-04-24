d1d880897d32c7611ce0dcbbc5f2ac324f6ceb56
graphica99 <mendsalbert@gmail.com>
2022-04-24 17:07:05 +0000

    my donate

--- components/DonateModal.tsx
+++ components/DonateModal.tsx
@@ -4,11 +4,11 @@ type Props = {};
 
 const DonateModal = (props: Props) => {
   return (
-    <div className="p-5 font-Montserrat">
+    <div className="p-5 font-Montserrat overflow-scroll">
       <p className="text-center text-gray-500 text-lg">
         How much you wanna donate?
       </p>
-      <div className="py-3 space-y-2">
+      <div className="py-3 space-y-3">
         <div className="text-center cursor-pointer border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-green-300 text-gray-700 ">
           5 ETH
         </div>
@@ -23,6 +23,9 @@ const DonateModal = (props: Props) => {
           className="w-full py-3 outline-none ring-2 rounded-lg p-3"
           placeholder="Enter amount manually"
         />
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 rounded-lg cursor-pointer text-white">
+          Continue
+        </div>
       </div>
     </div>
   );
