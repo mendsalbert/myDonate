aebfdb36677734b86fbcaa5ebf19ba93e91b5acc
graphica99 <mendsalbert@gmail.com>
2022-04-24 01:29:48 +0000

    my donate

--- pages/donation.tsx
+++ pages/donation.tsx
@@ -39,8 +39,8 @@ const Donation = () => (
 
     <div className=" mt-16">
       <p className="font-bold text-xl text-gray-500 my-10">Trending Category</p>
-      <div className="flex flex-col space-x-6 ">
-        <div className="8/12">
+      <div className="flex flex-col">
+        <div className="w-full">
           <div className="w-full rounded-lg">
             <img src="/images/war.jpg" className=" rounded-lg object-cover" />
           </div>
