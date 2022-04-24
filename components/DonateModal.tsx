5f65d9efca883ec7a1d9c6c5979ba38f1cf01961
graphica99 <mendsalbert@gmail.com>
2022-04-24 17:03:30 +0000

    my donate

--- components/DonateModal.tsx
+++ components/DonateModal.tsx
@@ -8,14 +8,14 @@ const DonateModal = (props: Props) => {
       <p className="text-center text-gray-500 text-lg">
         How much you wanna donate?
       </p>
-      <div className="">
-        <div className="text-center border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-green-300 text-gray-700 ">
+      <div className="py-3 space-y-2">
+        <div className="text-center cursor-pointer border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-green-300 text-gray-700 ">
           5 ETH
         </div>
-        <div className="text-center border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-blue-300 text-gray-700 ">
+        <div className="text-center cursor-pointer border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-blue-300 text-gray-700 ">
           10 ETH
         </div>
-        <div className="text-center border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-purple-300 text-gray-700 ">
+        <div className="text-center cursor-pointer border-[1px] border-opacity-30 text-lg border-gray-600 rounded-lg w-full py-2 bg-purple-300 text-gray-700 ">
           15 ETH
         </div>
       </div>
