f07d756c9dad144444aa1069ceacd5df6efa6b00
graphica99 <mendsalbert@gmail.com>
2022-04-25 15:39:06 +0000

    my donate

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,2 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+contract Donation {
+    
+    mapping(uint256 => DonationItem) private idToDonationItem;
+
+    struct Doners {
+      uint id;
+      uint amount;
+      uint date;
+    }
+
+    struct DonationItem {
+      uint256 id;
+      address payable owner;
+      uint256 amount;
+      Doners doners;
+      uint startDate;
+      uint endDate;
+      uint targetPrice;
+      string category;
+      string hash;
+      string description;
+      bool completed;
+    }
+
+    event DonationItemCreated (
+      uint256 indexed id,
+      address  owner,
+      uint256 amount,
+      Doners doners,
+      uint startDate,
+      uint endDate,
+      uint targetPrice,
+      string category,
+      string hash,
+      string description,
+      bool completed
+    );
+
+
+}
