b60bd263d5904289827c0569122985efda01c72c
graphica99 <mendsalbert@gmail.com>
2022-04-25 18:17:43 +0000

    my donate

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.4;
 
 contract Donation {
 
-    mapping(uint256 => DonationItem) private idToDonationItem;
+    mapping(uint256 => DonationItem) public idToDonationItem;
+    mapping(uint => address) public doners;
+    uint public donersCount = 0;
     uint public donationCount = 0 ;
     struct Doners {
       uint id;
@@ -15,8 +17,6 @@ contract Donation {
       uint256 id;
       address payable owner;
       uint donationAmount;
-      mapping(uint => address) doners;
-      uint donersCount;
       uint startDate;
       uint endDate;
       uint targetPrice;
@@ -46,6 +46,7 @@ contract Donation {
     function uploadDonation(string memory _imageHash, string memory _description, uint _startDate, uint _endDate, string memory _category, string memory _title, uint _targetPrice) public {
         //make sure the image hash exist
         require(bytes(_imageHash).length > 0 ,'Image Hash is required');
+      
         //make sure image description exist
         require(bytes(_description).length > 0,'Image description is required');
         require(bytes(_category).length > 0,'Image description is required');
@@ -54,11 +55,11 @@ contract Donation {
         //make sure uploader address exist
         require(msg.sender != address(0x0));
         donationCount++;
+        donersCount++;
         DonationItem storage donation = idToDonationItem[donationCount++];
         donation.donationAmount = 0;
         donation.owner = payable(address(msg.sender));
-        donation.donersCount = 0;
-        donation.doners[0] = address(0x0);
+        doners[donationCount++] = address(0x0);
         donation.startDate = _startDate;
         donation.endDate = _endDate;
         donation.targetPrice = _targetPrice;
@@ -72,12 +73,20 @@ contract Donation {
 
     function addDonation(uint _id) public payable {
         require(_id > 0 && _id <= donationCount);
-        DonationItem memory _donation = idToDonationItem[_id];
+        doners[_id];
+        DonationItem storage _donation = idToDonationItem[_id];
+        require(_donation.completed == false);
+        //check date if it expired.
+        if(_donation.donationAmount >= _donation.targetPrice){
+            _donation.completed = true;
+        }
         address payable _owner = _donation.owner;
         _owner.transfer(msg.value);
-        _donation.am = _image.tipAmount + msg.value;
-        images[_id] = _image;
-        emit ImageTip(_id, _image.hash, _image.description, _image.tipAmount, _author);
+        _donation.donationAmount = _donation.donationAmount + msg.value;
+        donersCount = donationCount;
+        doners[donersCount] = address(msg.sender);
+        idToDonationItem[_id] = _donation;
+        // emit ImageTip(_id, _image.hash, _image.description, _image.tipAmount, _author);
     }
 
 }
