c88d259827c95f539d7d322883e546fdc83e06c4
graphica99 <mendsalbert@gmail.com>
2022-04-25 18:28:44 +0000

    my donate

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -6,7 +6,8 @@ contract Donation {
     mapping(uint256 => DonationItem) public idToDonationItem;
     mapping(uint => address) public doners;
     uint public donersCount = 0;
-    uint public donationCount = 0 ;
+    uint public donationCount = 0;
+
     struct Doners {
       uint id;
       uint amount;
@@ -42,8 +43,12 @@ contract Donation {
       bool completed
     );
 
+    event DonationTiped (
+      
+    );
+
     //create a new donation
-    function uploadDonation(string memory _imageHash, string memory _description, uint _startDate, uint _endDate, string memory _category, string memory _title, uint _targetPrice) public {
+    function uploadDonation(string memory _imageHash, string memory _description, uint _endDate, string memory _category, string memory _title, uint _targetPrice) public {
         //make sure the image hash exist
         require(bytes(_imageHash).length > 0 ,'Image Hash is required');
       
@@ -60,7 +65,7 @@ contract Donation {
         donation.donationAmount = 0;
         donation.owner = payable(address(msg.sender));
         doners[donationCount++] = address(0x0);
-        donation.startDate = _startDate;
+        donation.startDate = block.timestamp;
         donation.endDate = _endDate;
         donation.targetPrice = _targetPrice;
         donation.category = _category;
@@ -68,7 +73,7 @@ contract Donation {
         donation.hash = _imageHash;
         donation.description = _description;
         donation.completed = false;
-        emit DonationItemCreated(donationCount, payable(address(msg.sender)), 0, 0, _startDate, _endDate, _targetPrice, _category, _title, _imageHash, _description, false);
+        emit DonationItemCreated(donationCount, payable(address(msg.sender)), 0, 0, block.timestamp, _endDate, _targetPrice, _category, _title, _imageHash, _description, false);
     }
 
     function addDonation(uint _id) public payable {
@@ -76,10 +81,12 @@ contract Donation {
         doners[_id];
         DonationItem storage _donation = idToDonationItem[_id];
         require(_donation.completed == false);
+       
         //check date if it expired.
         if(_donation.donationAmount >= _donation.targetPrice){
             _donation.completed = true;
         }
+
         address payable _owner = _donation.owner;
         _owner.transfer(msg.value);
         _donation.donationAmount = _donation.donationAmount + msg.value;
