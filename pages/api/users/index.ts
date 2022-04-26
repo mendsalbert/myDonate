ced22d0bc052ef0c82c19217b2837366189444f1
graphica99 <mendsalbert@gmail.com>
2022-04-26 05:30:12 +0000

    my donate

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,16 +1,14 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { sampleUserData } from '../../../utils/sample-data'
+import { NextApiRequest, NextApiResponse } from "next";
+import { sampleUserData } from "../../../utils/sample-data";
+import https from "https";
 
+const url = "https://api.coinmarketcap.com/v2/ticker/1027/?convert=USD";
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data')
-    }
-
-    res.status(200).json(sampleUserData)
+    res.status(200).json(sampleUserData);
   } catch (err: any) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+    res.status(500).json({ statusCode: 500, message: err.message });
   }
-}
+};
 
-export default handler
+export default handler;
