9cacbbc2c03631ccf232fe78429de9d282406fa2
graphica99 <mendsalbert@gmail.com>
2022-04-27 11:17:51 +0000

    my donate

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const donationAddress = "0x99BAd59C8da9Dd0faB7715D0c06CCc2b33058f14";
+export const donationAddress = "0xe9018e8589b66c864716e18Bf29175C012663203";
 // export const donationAddress = "0x97d82ab599192d68D95373Fb474AA0fd59ffDF3F";
