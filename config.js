708524eb73c76a27a3a6bc2abaf2ef55ef4fd434
graphica99 <mendsalbert@gmail.com>
2022-05-05 12:07:34 +0000

    my donate

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // export const donationAddress = "0xe9018e8589b66c864716e18Bf29175C012663203";
 
 //second updated contract
-export const donationAddress = "0x4AF472Fd2AfDDc0BC718308841bC40bAFb902912";
+export const donationAddress = "0xbe83a12c45b7d35c9f943Da9713bf840AeA1eC43";
 
 //localhost contract addresse
 // export const donationAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
